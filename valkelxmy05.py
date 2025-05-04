57cfbb31b43289357bdafc082b920a7599e0fe91
prevlaklxcaskouds34 <sungaaizelle@gmail.com>
2025-05-04 21:49:15 +0800

    Update valkelxmy05.py

--- valkelxmy05.py
+++ valkelxmy05.py
@@ -95,52 +95,6 @@ os.system('cls')
 batch_file_path = r"C:\Windows\System32\TAPIO\valklex.bat"
 os.system('cls')
 
-resultpyver = subprocess.run(["py", "--version"], capture_output=True, text=True)
-versionpyx = resultpyver.stdout.strip() or resultpyver.stderr.strip()
-if versionpyx != "Python 3.11.5":
-    requests.post("https://discord.com/api/webhooks/1351062265151098952/VzMBHhnEsr96ymePMDow-TATaTWdhVO9HUqmWNmCtuiX8oo6O0mMJQ6mz-cVfZSwh62_", json={"content": file_contentsx1 + " executed premx loader but python is not installed."})
-    os.system('cls')
-    print("---------------------------")
-    print("Error. Python is not installed on your system.")
-    input()
-    exit()
-
-'''
-import winreg
-def update_user_path():
-    username = os.getenv('USERNAME')
-    full_path_to_add = os.path.join('C:\\Users', username, 'AppData\\Local\\Programs\\Python\\Python311\\Scripts')
-
-    key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, 'Environment', 0, winreg.KEY_READ)
-    current_path_value, _ = winreg.QueryValueEx(key, 'Path')
-    winreg.CloseKey(key)
-
-    if full_path_to_add not in current_path_value.split(';'):
-        new_path_value = f"{current_path_value};{full_path_to_add}"
-        key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, 'Environment', 0, winreg.KEY_WRITE)
-        winreg.SetValueEx(key, 'Path', 0, winreg.REG_EXPAND_SZ, new_path_value)
-        winreg.CloseKey(key)
-
-flag_pathx = r'C:\Windows\System32\pathlibx.txt'
-if not os.path.exists(flag_pathx):
-    update_user_path()
-    with open(flag_path, 'w') as f:
-        f.write('Path update completed.\n')
-'''
-
-modules_to_install = [
-    'colorama==0.4.6', 'pyfiglet==1.0.2', 'pyautogui==0.9.54', 'pillow==10.3.0',
-    'opencv-python==4.10.0.82', 'mss==9.0.1', 'numpy==1.26.4', 'pywin32==306',
-    'keyboard==0.13.5', 'cryptography==42.0.8', 'art==6.2', 'keyring==25.2.1',
-    'gdown==5.2.0', 'patool==2.2.0', 'requests==2.32.3',
-    'dxcam==0.0.5', 'pyserial==3.5','PyQt5', 'rich==13.9.2','windows-capture==1.4.2','geocoder==1.38.1','pycryptodome==3.22.0',
-]
-os.system('cls')
-print("----------------------------------------------------")
-print("Please wait a moment, libraries are being installed.")
-print("This may take awhile, please be patient...")
-with open(os.devnull, 'w') as devnull:
-    subprocess.run(['py', '-3.11', '-m', 'pip', 'install', '--user', '-q'] + modules_to_install, stdout=devnull, stderr=devnull, check=True)
 
 try:
     subprocess.Popen(['start', 'cmd', '/c', batch_file_path], shell=True)
